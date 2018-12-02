3aae6917a93edd02fe5756a4e043a5abc268b73a
creio <mailcreio@gmail.com>
2018-12-02 17:18:58 +0300

    2018.12.02 Update

---
+++ www/bitrix/index.php
@@ -0,0 +1,86 @@
+<?php
+ini_set( 'display_errors', 'off' );
+$error = false;
+$message = '';
+if ( isset( $_GET[ 'action' ] ) ) {
+    switch ( $_GET[ 'action' ] ) {
+        case 'phpinfo': {
+            phpinfo();
+            exit();
+            break;
+        }
+        case 'restore': {
+            $path = $_SERVER[ 'DOCUMENT_ROOT' ] . DIRECTORY_SEPARATOR . 'restore.php';
+            $url = 'http://www.1c-bitrix.ru/download/scripts/restore.php';
+            if ( !file_exists( $path ) ) {
+                $data = file_get_contents( $url );
+                if ( $data === false || file_put_contents( $path, $data ) === false ) {
+                    $error = true;
+                }
+            }
+            if ( !$error ) {
+                header( 'Location: http://' . $_SERVER[ 'HTTP_HOST' ] . '/restore.php' );
+                exit();
+            } else {
+                $message = 'Не удалось получить файл restore.php';
+            }
+            break;
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Docker Bitrix Dev</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <style>
+    body {
+        height: 100%;
+        width: 100%;
+        background: linear-gradient(to bottom, #957bbe 0%,#8e71bc 40%,#8867bc 100%);
+    }
+    h1 {
+        color: #fff;
+        text-align: center;
+        width: 400px;
+        height: 50px;
+    }
+    .wrap {
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+</head>
+<body>
+    <div class="wrap">
+        <h1>BitrixDock Online!</h1>
+
+        <div>
+            <?= $message ?>
+        </div>
+
+        <p>
+            <a href="/?action=phpinfo" target="_blank">PHP info</a><br>
+            <a href="/?action=restore">Восстановить 1С-Битрикс</a>
+        </p>
+        <p>
+            Полезные скрипты:
+        </p>
+        <ul>
+            <li><a href="http://www.1c-bitrix.ru/download/scripts/bitrixsetup.php">bitrixsetup.php</a></li>
+            <li><a href="http://www.1c-bitrix.ru/download/scripts/restore.php">restore.php</a></li>
+            <li><a href="http://dev.1c-bitrix.ru/download/scripts/bitrix_server_test.php">bitrix_server_test.php</a></li>
+        </ul>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
+</html>
